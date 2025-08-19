686f0ac61ad95cf852121717fec06a0e50a958b7
EPTecmi <edgaripatricio@tecmilenio.mx>
2025-08-19 13:14:50 -0600

    12.py

---
+++ 12.py
@@ -0,0 +1,7 @@
+# Validación de contraseña
+contraseña = ""
+while contraseña != "python123":
+    contraseña = input("Ingrese la contraseña: ")
+    if contraseña != "python123":
+        print("Contraseña incorrecta. Intente nuevamente.")
+print("¡Acceso concedido!")
